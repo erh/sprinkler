76bf8eee1f610ccd2647d9c1aed04ec96585c9d8
Eliot Horowitz <eliothorowitz@gmail.com>
2025-04-29 08:41:49 -0400

    update rdk

--- cmd/module/cmd.go
+++ cmd/module/cmd.go
@@ -1,40 +1,15 @@
 package main
 
 import (
-	"context"
-
 	"go.viam.com/rdk/components/sensor"
 	"go.viam.com/rdk/module"
+	"go.viam.com/rdk/resource"
 
 	"github.com/erh/sprinkler"
 )
 
 func main() {
-	err := realMain()
-	if err != nil {
-		panic(err)
-	}
-}
-func realMain() error {
-
-	ctx := context.Background()
-	logger := module.NewLoggerFromArgs("sprinklermod")
-
-	myMod, err := module.NewModuleFromArgs(ctx, logger)
-	if err != nil {
-		return err
-	}
-
-	err = myMod.AddModelFromRegistry(ctx, sensor.API, sprinkler.SprinklerModel)
-	if err != nil {
-		return err
-	}
-
-	err = myMod.Start(ctx)
-	defer myMod.Close(ctx)
-	if err != nil {
-		return err
-	}
-	<-ctx.Done()
-	return nil
+	module.ModularMain(
+		resource.APIModel{sensor.API, sprinkler.SprinklerModel},
+	)
 }
